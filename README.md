46d2d43c86d8f886318f504887592daa807c0734
KainoVL <stredalt@gmail.com>
2024-07-15 16:23:34 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+this is just for my own personal stuff
+
+
+
+```
+getgenv().whitelist = {USERID} --this is for grass touching simulator when there are only whitelisted players in your server it will set your walkspeed to 100 and if there are people who aren't whitelisted in your server it will set it to 20. Put their roblox user id
+getgenv().AuthKey = "KEY" --for pet simulator 99 get your key from https://hugegames.io/
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Rivkaaaazz/meow/main/meow.lua",true))()
+```
+## Scripts in meow
+
+## [Supports All Executors](https://robloxexploits.site/executors)
+
+### [AutoWalk](https://github.com/Rivkaaaazz/meow/blob/main/autowalk.lua) - [Grass Touching Simulator](https://www.roblox.com/games/8387888847/Grass-Touching-Simulator)
+### [MeowGUI](https://github.com/Rivkaaaazz/meow/blob/main/meowgui.lua) - [Nico's Nextbots Open World AFK Farm](https://www.roblox.com/games/10118559731/nicos-nextbots)
+### [Aimbot/ESP](https://github.com/Mick-gordon/Hyper-Escape) - [Arsenal](https://www.roblox.com/games/286090429/Arsenal)
+
+## [Supports Wave](https://getwave.gg/) - [and android maybe?](https://robloxexploits.site/executors)
+
+### [HoHo Hub](https://discord.com/invite/hohohub) - [Blox Fruits](https://www.roblox.com/games/2753915549/Blox-Fruits)
+### [HUGEGames](https://hugegames.io/) - [Pet Simulator 99](https://www.roblox.com/games/8737899170/Pet-Simulator-99)
+### [Plutonium](https://discord.com/invite/h63n2kAEnr) - [Blade Ball](https://www.roblox.com/games/13772394625/UPD-Blade-Ball)
+
+Note: This is based off testing with PC executors and have not been tested on Android devices.
---
+++ autowalk.lua
@@ -0,0 +1,108 @@
+spawn(function()
+    while wait() do
+        game:GetService("ReplicatedStorage").FoundFriend:FireServer(5)
+    end
+end)
+
+local game = game
+local players = game.Players
+local whitelist = getgenv().whitelist or {}
+
+local grassHierarchy = {
+    "Quantum", "Negative", "Celestial", "Moon", "Solar", "Sun", "Midas", "Rainbow",
+    "Plasma", "Diamond", "Emerald", "Ruby", "Sapphire", "Amethyst", "Gold", "Silver", "Bronze", "Green"
+}
+
+local function isPlayerWhitelisted(player)
+    for _, id in ipairs(whitelist) do
+        if player.UserId == id then
+            return true
+        end
+    end
+    return false
+end
+
+local function setWalkSpeed(speed)
+    for _, player in ipairs(players:GetPlayers()) do
+        local character = player.Character
+        if character then
+            local humanoid = character:FindFirstChildOfClass("Humanoid")
+            if humanoid then
+                humanoid.WalkSpeed = speed
+            end
+        end
+    end
+end
+
+local function findBestGrass()
+    for _, grassType in ipairs(grassHierarchy) do
+        local grassParts = Workspace.Grass:GetChildren()
+        local bestGrassParts = {}
+        for _, part in ipairs(grassParts) do
+            if part.Name == grassType then
+                table.insert(bestGrassParts, part)
+            end
+        end
+        if #bestGrassParts > 0 then
+            return bestGrassParts
+        end
+    end
+    return {}
+end
+
+local lastPositions = {}
+
+local function moveWhitelistedPlayersToGrass()
+    local bestGrassParts = findBestGrass()
+    if #bestGrassParts == 0 then return end
+
+    for _, player in ipairs(players:GetPlayers()) do
+        if isPlayerWhitelisted(player) then
+            local character = player.Character
+            if character then
+                local humanoid = character:FindFirstChildOfClass("Humanoid")
+                local rootPart = character:FindFirstChild("HumanoidRootPart")
+                if humanoid and rootPart then
+                    local currentPosition = rootPart.Position
+                    local lastPosition = lastPositions[player.UserId]
+                    
+                    if not lastPosition or (currentPosition - lastPosition).Magnitude > 1 then
+                        local randomGrass
+                        local attempts = 0
+                        repeat
+                            randomGrass = bestGrassParts[math.random(1, #bestGrassParts)]
+                            attempts = attempts + 1
+                        until (randomGrass.Position - currentPosition).Magnitude > 10 or attempts > 10
+                        
+                        humanoid:MoveTo(randomGrass.Position + Vector3.new(0, 3, 0))
+                        lastPositions[player.UserId] = randomGrass.Position
+                    end
+                end
+            end
+        end
+    end
+end
+
+local function areNonWhitelistedPlayersPresent()
+    for _, player in ipairs(players:GetPlayers()) do
+        if not isPlayerWhitelisted(player) then
+            return true
+        end
+    end
+    return false
+end
+
+local defaultSpeed = 20
+local whitelistedSpeed = 100
+
+while true do
+    if areNonWhitelistedPlayersPresent() then
+        setWalkSpeed(defaultSpeed)
+    else
+        setWalkSpeed(whitelistedSpeed)
+    end
+
+    moveWhitelistedPlayersToGrass()
+
+    wait(1)
+end
